0656b0eb8dc1210bff42f07766bddb1a365c7782
yair-roshal <yair7705778@gmail.com>
2023-05-28 17:26:46 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useState, useEffect, useCallback } from 'react'
 import './App.scss'
-import Card from './components/Card/Card'
-import Cart from './components/Cart/Cart'
+// eslint-disable-next-line import/no-unresolved
 import Form from './components/Form/Form'
-import Header from './components/Header/Header'
 import ProductList from './components/ProductList/ProductList'
 import OrderPage from './components/OrderPage/OrderPage'
-import { Link } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 
 const { getData } = require('./db/db')
