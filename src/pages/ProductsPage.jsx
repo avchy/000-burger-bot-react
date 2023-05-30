9f4bac7b0bc94faf56795bd5cec87c2176a2fdd7
yair-roshal <yair7705778@gmail.com>
2023-05-30 21:22:11 +0300

    fix

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -21,22 +21,27 @@ export const ProductsPage = () => {
             const ua = navigator.userAgent
             let tem
             let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
+
             if (/trident/i.test(M[1])) {
                 tem = /\brv[ :]+(\d+)/g.exec(ua) || []
+                setEnv('brow')
                 return 'IE ' + (tem[1] || '')
             }
+
             if (M[1] === 'Chrome') {
                 tem = ua.match(/\b(OPR|Edge)\/(\d+)/)
-                if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera')
+                if (tem != null) {
+                    setEnv('brow')
+                    return tem.slice(1).join(' ').replace('OPR', 'Opera')
+                }
             }
             M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?']
+            M = M[2] ? setEnv('brow') : setEnv('tele')
             if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1])
             return M.join(' ')
         })()
 
         console.log('navigator.sayswho', navigator.sayswho)
-
-        setEnv(navigator.sayswho)
     }, [])
 
     const { tele } = useTelegram()
