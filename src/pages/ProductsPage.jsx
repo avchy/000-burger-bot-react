0ebea4549b1225243d0e43da20b5ff60e46a0109
yair-roshal <yair7705778@gmail.com>
2023-05-30 21:40:21 +0300

    fix

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,6 +3,7 @@ import '../App.scss'
 import { CardColumn } from '../components/CardColumn'
 import { Button } from '../components/Button'
 import { useTelegram } from '../hooks/useTelegram'
+import { useNavigator } from '../hooks/useNavigator'
 import { Link, useNavigate } from 'react-router-dom'
 
 const { getData } = require('../db/db')
@@ -15,36 +16,37 @@ console.log('tele.MainButton :>> ', tele.MainButton)
 tele.MainButton.text = 'VIEW ORDER'
 
 export const ProductsPage = () => {
-    const [env, setEnv] = useState('init')
-    useEffect(() => {
-        navigator.sayswho = (function () {
-            const ua = navigator.userAgent
-            let tem
-            let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
+    // const [env, setEnv] = useState('init')
+    // useEffect(() => {
+    //     navigator.sayswho = (function () {
+    //         const ua = navigator.userAgent
+    //         let tem
+    //         let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
 
-            if (/trident/i.test(M[1])) {
-                tem = /\brv[ :]+(\d+)/g.exec(ua) || []
-                setEnv('brow')
-                return 'IE ' + (tem[1] || '')
-            }
+    //         if (/trident/i.test(M[1])) {
+    //             tem = /\brv[ :]+(\d+)/g.exec(ua) || []
+    //             setEnv('brow')
+    //             return 'IE ' + (tem[1] || '')
+    //         }
 
-            if (M[1] === 'Chrome') {
-                tem = ua.match(/\b(OPR|Edge)\/(\d+)/)
-                if (tem != null) {
-                    setEnv('brow')
-                    return tem.slice(1).join(' ').replace('OPR', 'Opera')
-                }
-            }
-            M[2] ? setEnv('brow') : setEnv('tele')
-            M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?']
-            if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1])
-            return M.join(' ')
-        })()
+    //         if (M[1] === 'Chrome') {
+    //             tem = ua.match(/\b(OPR|Edge)\/(\d+)/)
+    //             if (tem != null) {
+    //                 setEnv('brow')
+    //                 return tem.slice(1).join(' ').replace('OPR', 'Opera')
+    //             }
+    //         }
+    //         M[2] ? setEnv('brow') : setEnv('tele')
+    //         M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?']
+    //         if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1])
+    //         return M.join(' ')
+    //     })()
 
-        console.log('navigator.sayswho', navigator.sayswho)
-    }, [])
+    //     console.log('navigator.sayswho', navigator.sayswho)
+    // }, [])
 
     const { tele } = useTelegram()
+    const { env } = useNavigator()
 
     const [cartItems, setCartItems] = useState([])
     const navigate = useNavigate()
@@ -107,8 +109,8 @@ export const ProductsPage = () => {
     return (
         <div className='productsPage'>
             <h1 className='title'>Burger Shop</h1>
-            <h1 className='title'> navigator.sayswho = {navigator.sayswho}</h1>
-            <h1 className='title'>env = {env}</h1>
+            {/* <h1 className='title'> navigator.sayswho = {navigator.sayswho}</h1> */}
+            <h1 className='title'>env222 = {env}</h1>
 
             <div className='cards_container'>
                 {foods.map((food) => {
