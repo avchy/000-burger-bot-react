cac9bc3b0305aa132e483eb111aaecbbd6963953
yair-roshal <yair7705778@gmail.com>
2023-07-07 01:28:04 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,20 @@ import { useTelegram } from "../hooks/useTelegram"
 import "../App.scss"
 
 import generatedGitInfo from "../helpers/generatedGitInfo.json"
-const { gitCommitHash } = generatedGitInfo
+const { gitCommitHash, gitTimestampDate, gitTimestamp } = generatedGitInfo
 
 export const Header = () => {
   const { user, onClose } = useTelegram()
-  
-  const query_id = window.Telegram.WebApp.initDataUnsafe.query_id;
+
+  const query_id = window.Telegram.WebApp.initDataUnsafe.query_id
 
   return (
     <div className={"header"}>
+      <p className={"testText"}> {`   gitTimestampDate - ${gitTimestampDate}`} </p>
+      <p className={"testText"}> {`   gitTimestamp - ${gitTimestamp}`} </p>
       <p className={"testText"}> {`   gitCommitHash - ${gitCommitHash}`} </p>
       <p className={"testText"}> {`   query_id - ${query_id}`} </p>
- 
+
       {/* <BigButton onClick={onClose}>Close</BigButton> */}
       {/* <span className={"username"}>{user?.username}</span> */}
     </div>
--- src/helpers/gitInfo.js
+++ src/helpers/gitInfo.js
@@ -1,33 +1,36 @@
-const fs = require('fs');
-const path = require('path');
-const {execSync} = require('child_process');
+const fs = require("fs")
+const path = require("path")
+const { execSync } = require("child_process")
 
 const execSyncWrapper = (command) => {
-  let stdout = null;
+  let stdout = null
   try {
-    stdout = execSync(command).toString().trim();
+    stdout = execSync(command).toString().trim()
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-  return stdout;
-};
+  return stdout
+}
 
 const main = () => {
-  let gitBranch = execSyncWrapper('git rev-parse --abbrev-ref HEAD');
-  let gitCommitHash = execSyncWrapper('git rev-parse --short=7 HEAD');
-
+  const gitBranch = execSyncWrapper("git rev-parse --abbrev-ref HEAD")
+  const gitCommitHash = execSyncWrapper("git rev-parse --short=7 HEAD")
+  const gitTimestamp = execSyncWrapper("git log -1 --format=%ct")
+  const gitTimestampDate = new Date(gitTimestamp * 1000)
   const obj = {
     gitBranch,
-    gitCommitHash
-  };
+    gitCommitHash,
+    gitTimestamp,
+    gitTimestampDate,
+  }
 
   // const filePath = path.resolve('src', 'generatedGitInfo.json');
-  const filePath = path.resolve('src/helpers', 'generatedGitInfo.json');
+  const filePath = path.resolve("src/helpers", "generatedGitInfo.json")
 
-  const fileContents = JSON.stringify(obj, null, 2);
+  const fileContents = JSON.stringify(obj, null, 2)
 
-  fs.writeFileSync(filePath, fileContents);
-  console.log(`Wrote the following contents to ${filePath}\n${fileContents}`);
-};
+  fs.writeFileSync(filePath, fileContents)
+  console.log(`Wrote the following contents to ${filePath}\n${fileContents}`)
+}
 
-main();
+main()
