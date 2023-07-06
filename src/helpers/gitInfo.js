b63cd2a3ce144dd8519fe5ee7fdc14341f9acf44
yair-roshal <yair7705778@gmail.com>
2023-07-07 02:04:13 +0300

    fix

--- src/helpers/gitInfo.js
+++ src/helpers/gitInfo.js
@@ -13,37 +13,31 @@ const execSyncWrapper = (command) => {
 }
 
 const main = () => {
- 
-  
-  const gitTimestamp000 = execSyncWrapper("git log -1 --format=%ct");
-const timestampSeconds = parseInt(gitTimestamp000.trim());
+  const gitTimestamp000 = execSyncWrapper("git log -1 --format=%ct")
+  const timestampSeconds = parseInt(gitTimestamp000.trim())
 
-const date = new Date(timestampSeconds * 1000);
-const hours1 = date.getHours().toString().padStart(2, "0");
-const minutes1 = date.getMinutes().toString().padStart(2, "0");
-const seconds1 = date.getSeconds().toString().padStart(2, "0");
+  const date = new Date(timestampSeconds * 1000)
+  const hours1 = date.getHours().toString().padStart(2, "0")
+  const minutes1 = date.getMinutes().toString().padStart(2, "0")
+  const seconds1 = date.getSeconds().toString().padStart(2, "0")
 
-const gitTimestamp = `${hours1}:${minutes1}:${seconds1}`;
-
+  const gitTimestamp = `${hours1}:${minutes1}:${seconds1}`
 
   //=======================
-  
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-const gitTimestampDate1 = new Date(currentTimestamp * 1000);
 
-  
- const hours = gitTimestampDate1.getHours().toString().padStart(2, '0');
-const minutes = gitTimestampDate1.getMinutes().toString().padStart(2, '0');
-const seconds = gitTimestampDate1.getSeconds().toString().padStart(2, '0');
+  const currentTimestamp = Math.floor(Date.now() / 1000)
+  const gitTimestampDate1 = new Date(currentTimestamp * 1000)
 
-const gitTimestampDate = `${hours}:${minutes}:${seconds}`;
- 
+  const hours = gitTimestampDate1.getHours().toString().padStart(2, "0")
+  const minutes = gitTimestampDate1.getMinutes().toString().padStart(2, "0")
+  const seconds = gitTimestampDate1.getSeconds().toString().padStart(2, "0")
 
+  const gitTimestampDate = `${hours}:${minutes}:${seconds}`
 
   const gitBranch = execSyncWrapper("git rev-parse --abbrev-ref HEAD")
   const gitCommitHash = execSyncWrapper("git rev-parse --short=7 HEAD")
-  
-   const obj = {
+
+  const obj = {
     gitBranch,
     gitCommitHash,
     gitTimestamp,
