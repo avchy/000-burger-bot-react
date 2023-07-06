3d3fb9fb8e06888a7947c43209576e6301561a35
yair-roshal <yair7705778@gmail.com>
2023-07-06 21:42:08 +0300

    fix

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
-const serverIP = 'http://54.86.166.140'
+const serverIP = 'http://94.198.216.20'
+// const serverIP = 'http://54.86.166.140'
 const port = '8000'
 
 module.exports = { serverIP, port }
