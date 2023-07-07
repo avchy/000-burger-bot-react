6d4451b25e9967fd9fc8e982c8ec3129342c70e1
yair-roshal <yair7705778@gmail.com>
2023-07-07 18:45:36 +0300

    fix

--- src/helpers/gitInfo.js
+++ src/helpers/gitInfo.js
@@ -13,27 +13,26 @@ const execSyncWrapper = (command) => {
 }
 
 const main = () => {
-  // const gitTimestamp000 = execSyncWrapper("git log -1 --format=%ct")
-  // const timestampSeconds = parseInt(gitTimestamp000.trim())
+  const gitTimestamp000 = execSyncWrapper("git log -1 --format=%ct")
+  const timestampSeconds = parseInt(gitTimestamp000.trim())
 
-  // const date = new Date((timestampSeconds+ 3 * 60 * 60) * 1000)
-  // const hours1 = (date.getHours() ).toString().padStart(2, "0")
-  // const minutes1 = date.getMinutes().toString().padStart(2, "0")
-  // const seconds1 = date.getSeconds().toString().padStart(2, "0")
-  
-  // const gitTimestamp = `${hours1}:${minutes1}:${seconds1}`
-  
+  const date = new Date((timestampSeconds + 3 * 60 * 60) * 1000)
+  const hours1 = date.getHours().toString().padStart(2, "0")
+  const minutes1 = date.getMinutes().toString().padStart(2, "0")
+  const seconds1 = date.getSeconds().toString().padStart(2, "0")
+
+  const gitTimestamp = `${hours1}:${minutes1}:${seconds1}`
+
   //=======================
-  
+
   const currentTimestamp = Math.floor(Date.now() / 1000)
   const gitTimestampDate1 = new Date((currentTimestamp + 3 * 60 * 60) * 1000)
-  
+
   const hours = gitTimestampDate1.getHours().toString().padStart(2, "0")
   const minutes = gitTimestampDate1.getMinutes().toString().padStart(2, "0")
   const seconds = gitTimestampDate1.getSeconds().toString().padStart(2, "0")
-  
+
   const gitTimestampDate = `${hours}:${minutes}:${seconds}`
-  
 
   const gitBranch = execSyncWrapper("git rev-parse --abbrev-ref HEAD")
   const gitCommitHash = execSyncWrapper("git rev-parse --short=7 HEAD")
@@ -41,13 +40,11 @@ const main = () => {
   const obj = {
     gitBranch,
     gitCommitHash,
-    // gitTimestamp,
+    gitTimestamp,
     gitTimestampDate,
   }
 
-  // const filePath = path.resolve('src', 'generatedGitInfo.json');
   const filePath = path.resolve("src/helpers", "generatedGitInfo.json")
-
   const fileContents = JSON.stringify(obj, null, 2)
 
   fs.writeFileSync(filePath, fileContents)
