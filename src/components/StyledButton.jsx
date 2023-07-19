5cbacdc8341975ec3702a5566e195f11a01d21f0
yair-roshal <yair7705778@gmail.com>
2023-07-19 21:31:06 +0300

    fix

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -5,13 +5,14 @@ export const StyledButton = styled(Button)(
   ({ theme }) => ({
     backgroundColor: "#000000",
     cursor: "pointer",
-    width: "100%",
-    height: "100%",
+    width: "90%",
+    height: "50px",
     color: "white",
-    padding: "10px 20px",
+    padding: "50px 20px",
+    margin: "20px 20px",
     boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
     textAlign: "center",
-    gap: 2,
+    gap: 5,
 
     fontSize: "20px",
     fontWeight: "bold",
--- src/components/pages/Payments.jsx
+++ src/components/pages/Payments.jsx
@@ -8,8 +8,7 @@ import {
   FormLabel,
   Button,
 } from "@mui/material"
-import { FlexColumnContainer,Layout } from "components/AllHelpComponents"
-
+import { FlexColumnContainer, Layout } from "components/AllHelpComponents"
 
 import { BigButton } from "components/BigButton"
 import { StyledButton } from "components/StyledButton"
@@ -21,29 +20,24 @@ const tele = window.Telegram.WebApp
 export function Payments() {
   const [paymentMethod, setPaymentMethod] = useState("")
   const navigate = useNavigate()
-  
- 
-
 
   const location = useLocation()
   const state = location?.state
-  console.log('state_in_payments', state)
+  console.log("state_in_payments", state)
   // const { cartItems, comment, totalPrice } = state
   // const data = { cartItems, comment, totalPrice }
 
   const onCreditCard = useCallback(() => {
-    console.log('data333', state)
+    console.log("data333", state)
     navigate("/creditCard", { state: state })
   }, [state])
 
-  
   const onBackButtonClicked = useCallback(() => {
     navigate(-1)
   }, [state])
-  
+
   tele.BackButton.onClick(onBackButtonClicked)
-  
-  
+
   const onApplePay = () => {
     tele.sendData(JSON.stringify(state))
   }
@@ -53,9 +47,16 @@ export function Payments() {
 
   return (
     <>
-      
       <FlexColumnContainer
-        sx={{ pt: "20px", backgroundColor: "#404040", gap: 2 }}
+        sx={{
+          pt: "20px",
+          backgroundColor: "#404040",
+          gap: 2,
+          height:
+            "100vh" /* Set the container's height to 100% of the viewport height */,
+          justifyContent: "center" /* Vertically center the content */,
+          alignItems: "center" /* Horizontally center the content */,
+        }}
       >
         <StyledButton onClick={onCreditCard} variant="contained">
           Credit Card
