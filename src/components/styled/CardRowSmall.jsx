9d8e34cb7a4b838913981d477aa7be0492b4341d
yair-roshal <yair7705778@gmail.com>
2023-07-21 01:40:23 +0300

    fix

--- src/components/styled/CardRowSmall.jsx
+++ src/components/styled/CardRowSmall.jsx
@@ -1,21 +1,34 @@
-import '../../App.scss'
+import "../../App.scss"
 
 export function CardRowSmall({ food }) {
-    const { title, Image, price, quantity } = food
+  const { title, Image, price, quantity } = food
 
-    const priceAllItems = (price * (quantity || 1)).toFixed(2)
+  const priceAllItems = (price * (quantity || 1)).toFixed(2)
+
+  return (
+    <div className="CardRowSmall">
+        
+        
+<div className="CardRowSmall">
+            
+          {Image && (
+    <div className="image_container">
+      <img src={Image} alt={title} />
+    </div>
+          )}
     
-    return (
-        <div className='CardRowSmall'>
-            {Image && (
-                <div className='image_container'>
-                    <img src={Image} alt={title} />
-                </div>
-            )}
+          <span className="cart_text_center">{title}</span>
+          {/* <span className="cart_text_center left">{title}</span> */}
+</div>
 
-            <span className='cart_text_center'>{title}</span>
-         
-          {   <span className='cart_text_center'> {price ? priceAllItems: "0.00"} $</span> } 
-        </div>
-    )
+
+
+      {
+        <span className="cart_text_center" >
+          {quantity && quantity !== 1 ? `x ${quantity} = ` : ""}
+          {price ? priceAllItems : "0.00"} ₪
+        </span>
+      }
+    </div>
+  )
 }
