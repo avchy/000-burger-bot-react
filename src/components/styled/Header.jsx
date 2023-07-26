f8abd83837596dc781c1c9866200391f04662038
yair-roshal <yair7705778@gmail.com>
2023-07-26 15:58:35 +0300

    fix

--- src/components/styled/Header.jsx
+++ src/components/styled/Header.jsx
@@ -87,14 +87,17 @@ export const Header = () => {
         </Button>
       </FlexRowContainer>
 
+
       <p className="testText">{`user?.language_code ${user?.language_code}`}</p>
+      
+      {/* 
 
       <p className={"testText"}>
         {` initDataUnsafe - ${JSON.stringify(tele?.initDataUnsafe, null, 2)}`}{" "}
       </p>
       <p className={"testText"}>
         {` WebApp - ${JSON.stringify(WebApp, null, 2)}`}{" "}
-      </p>
+      </p> */}
       
       {/* 
     
