aa48a086a65c3be99e8c1b201bdfd5ae1ea1f05e
yair-roshal <yair7705778@gmail.com>
2023-07-26 17:20:28 +0300

    update OrderPage.jsx and Header.jsx

--- src/components/styled/CardColumn.jsx
+++ src/components/styled/CardColumn.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
+import { Skeleton } from "@mui/material"
 import "../../App.scss"
 import { Button } from "./Button"
 import { useTranslation } from "react-i18next"
 
 export function CardColumn({ food, onAdd, onRemove, quantity }) {
   const [count, setCount] = useState(quantity || 0)
+  const [imageLoaded, setImageLoaded] = useState(false) // State to track if the image has loaded
   const { title, Image, price, id } = food
   const { t, i18n } = useTranslation()
 
@@ -18,18 +20,36 @@ export function CardColumn({ food, onAdd, onRemove, quantity }) {
     onRemove(food)
   }
 
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+  }
+
   return (
     <div className="CardColumn">
       <span className={`${count !== 0 ? "card_badge" : "card_badge--hidden"}`}>
         {count}
       </span>
-      <div className="image_container">
-        <img src={Image} alt={title} />
+
+      {/* Show Skeleton while image is loading */}
+      {!imageLoaded && <Skeleton variant="rounded" />}
+      {/* {!imageLoaded && <Skeleton variant="rounded" width={210} height={118} /> } */}
+
+      {/* Show the image after it has loaded */}
+      <div
+        className="image_container"
+        style={{ display: imageLoaded ? "block" : "none" }}
+      >
+        <img
+          src={Image}
+          alt={title}
+          onLoad={handleImageLoad}
+          style={{ display: imageLoaded ? "block" : "none" }}
+        />
       </div>
 
       {/* <h4 className="card_title">{title}</h4> */}
       <h4 className="card_title">{t(title)}</h4>
- 
+
       <p className="cart_text_center">{price} ₪</p>
 
       <div className="btn-container">
