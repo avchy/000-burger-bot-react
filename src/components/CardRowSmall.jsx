ae73e0e8fdf62e7d3bd5dfa1a299c9dd28d06fa6
yair-roshal <yair7705778@gmail.com>
2023-11-17 12:03:24 +0200

    update App.scss, CardRowSmall.jsx and OrderPage.jsx

--- src/components/CardRowSmall.jsx
+++ src/components/CardRowSmall.jsx
@@ -1,96 +1,96 @@
-import "App.scss"
-import { useTranslation } from "react-i18next"
-import { Box } from "@mui/material"
+import "App.scss";
+import { useTranslation } from "react-i18next";
+import { Box } from "@mui/material";
 
-import default_dish_img from "images/svg_dishes/pot-dinner-svgrepo-com.svg"
-import toppings_icon from "images/toppings_icon.png"
-import isPhotoUrl from "helpers/isPhotoUrl"
+import default_dish_img from "images/svg_dishes/pot-dinner-svgrepo-com.svg";
+import toppings_icon from "images/toppings_icon.png";
+import isPhotoUrl from "helpers/isPhotoUrl";
 
 export function CardRowSmall({ food }) {
-  console.log("food222", food)
-  const { title, image, price, quantity, textColor, toppings, selectedToppings } = food
-  const priceAllItems = (price * (quantity || 1)).toFixed(2)
-  const { t, i18n } = useTranslation()
+	console.log("food222", food);
+	const { title, image, price, quantity, textColor, toppings, selectedToppings } = food;
+	const priceAllItems = (price * (quantity || 1)).toFixed(2);
+	const { t, i18n } = useTranslation();
 
-  const calculateTotalPrice = () => {
-    let totalPrice = parseFloat(priceAllItems) // Инициализируем сумму исходной ценой товара
+	const calculateTotalPrice = () => {
+		let totalPrice = parseFloat(priceAllItems); // Инициализируем сумму исходной ценой товара
 
-    // Добавляем стоимость выбранных топпингов, если они есть
-    if (selectedToppings && selectedToppings.length > 0) {
-      selectedToppings.forEach((selectedTopping) => {
-        const topping = toppings.find((t) => t.title.trim() === selectedTopping.trim())
-        if (topping) {
-          totalPrice += topping.price * (quantity || 1)
-        }
-      })
-    }
+		// Добавляем стоимость выбранных топпингов, если они есть
+		if (selectedToppings && selectedToppings.length > 0) {
+			selectedToppings.forEach((selectedTopping) => {
+				const topping = toppings.find((t) => t.title.trim() === selectedTopping.trim());
+				if (topping) {
+					totalPrice += topping.price * (quantity || 1);
+				}
+			});
+		}
 
-    return totalPrice.toFixed(2) // Возвращаем общую цену с округлением до двух знаков после запятой
-  }
+		return totalPrice.toFixed(2); // Возвращаем общую цену с округлением до двух знаков после запятой
+	};
 
-  return (
-    <>
-      <div className="CardRowSmall">
-        <div className="CardRowSmall">
-          {image && (
-            <div className="image_container" style={{ width: "100px" }}>
-              <img src={isPhotoUrl(image) ? image : default_dish_img} alt={title} />
-            </div>
-          )}
-          <span className="cart_text_center">{t(title)}</span>
-        </div>
-        {
-          <span className="cart_text_center" style={{ color: textColor }}>
-            {quantity && quantity !== 1 ? `${price} x ${quantity} = ` : ""}
-            {price ? priceAllItems : "0.00"} ₪
-          </span>
-        }
-      </div>
-      {console.log("selectedToppings", selectedToppings)}
-      {console.log("selectedToppings?.length", selectedToppings?.length)}{" "}
-      {selectedToppings?.length > 0 &&
-        selectedToppings.map((selectedTopping, index) => {
-          const topping = toppings.find((t) => t.title.trim() === selectedTopping.trim())
+	return (
+		<>
+			<div className="CardRowSmall">
+				<div className="CardRowSmall">
+					{image && (
+						<div className="image_container" style={{ width: "100px" }}>
+							<img src={isPhotoUrl(image) ? image : default_dish_img} alt={title} />
+						</div>
+					)}
+					<span className="cart_text_center">{t(title)}</span>
+				</div>
+				{
+					<span className="cart_text_center" style={{ color: textColor }}>
+						{quantity && quantity !== 1 ? `${price} x ${quantity} = ` : ""}
+						{price ? priceAllItems : "0.00"} ₪
+					</span>
+				}
+			</div>
+			{console.log("selectedToppings", selectedToppings)}
+			{console.log("selectedToppings?.length", selectedToppings?.length)}{" "}
+			{selectedToppings?.length > 0 &&
+				selectedToppings.map((selectedTopping, index) => {
+					const topping = toppings.find((t) => t.title.trim() === selectedTopping.trim());
 
-          console.log("selectedTopping555", selectedTopping)
-          console.log("topping555", topping)
+					console.log("selectedTopping555", selectedTopping);
+					console.log("topping555", topping);
 
-          if (!topping) return null
+					if (!topping) return null;
 
-          return (
-            <div
-              className="CardRowSmall"
-              style={{ margin: "0 10px" }}
-              key={topping.title + index}
-            >
-              <div className="CardRowSmall">
-                <div className="image_container">
-                  <img
-                    src={isPhotoUrl(topping.image) ? topping.image : toppings_icon}
-                    alt={topping.title}
-                  />
-                </div>
-                <span className="card_row_text">{t(topping.title)}</span>
-              </div>
+					return (
+						<div
+							className="CardRowSmall"
+							style={{ margin: "0 10px" }}
+							key={topping.title + index}
+						>
+							<div className="CardRowSmall">
+								<div className="image_container">
+									<img
+										src={isPhotoUrl(topping.image) ? topping.image : toppings_icon}
+										alt={topping.title}
+									/>
+								</div>
+								<span className="card_row_text">{t(topping.title)}</span>
+							</div>
 
-              <span className="card_row_text">
-                {quantity !== 1 ? `${topping.price} x ${quantity} = ` : ""}
-                {(topping.price * (quantity || 1)).toFixed(2)} ₪
-              </span>
-            </div>
-          )
-        })}
-      {title != "Total Price:" && (
-        <div className="CardRowSmall">
-          <span className="cart_text_center">{t("Dish Price:")}</span>
-          <span className="cart_text_center" style={{ color: textColor }}>
-            {calculateTotalPrice()} ₪
-          </span>
-        </div>
-      )}
-      {title != "Total Price:" && (
-        <Box sx={{ width: "100%", height: "1px", backgroundColor: "grey" }}></Box>
-      )}
-    </>
-  )
+							<span className="card_row_text">
+								{quantity !== 1 ? `${topping.price} x ${quantity} = ` : ""}
+								{(topping.price * (quantity || 1)).toFixed(2)} ₪
+							</span>
+						</div>
+					);
+				})}
+			{title != "Total Price:" && (
+				<div className="CardRowSmall">
+					<span className="cart_text_center">{t("Dish Price:")}</span>
+					<span className="cart_text_center" style={{ color: textColor }}>
+						{calculateTotalPrice()} ₪
+					</span>
+				</div>
+			)}
+			{title != "Total Price:" && (
+				<Box sx={{ width: "100%", height: "1px", backgroundColor: "grey" }}></Box>
+			)}
+		</>
+	);
 }
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,187 +1,164 @@
-import "App.scss"
-import { useState, useEffect, useCallback, useContext } from "react"
-import { Link, useLocation, useNavigate } from "react-router-dom"
+import "App.scss";
+import { useState, useEffect, useCallback, useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  makeStyles,
-  Box,
-} from "@mui/material"
+	TextField,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	makeStyles,
+	Box,
+} from "@mui/material";
 
-import { BigButton } from "components/BigButton"
-import { StyledTextField } from "components/StyledTextField"
-import { CardRowSmall } from "components/CardRowSmall"
-import orderImg from "images/Cafe_Cafe_Logo.png"
+import { BigButton } from "components/BigButton";
+import { StyledTextField } from "components/StyledTextField";
+import { CardRowSmall } from "components/CardRowSmall";
+import orderImg from "images/Cafe_Cafe_Logo.png";
 // import orderImg from "images/orderImg.png"
-import { useNavigator } from "hooks/useNavigator"
-import { CartContext } from "App"
-import { useTranslation } from "react-i18next"
-
+import { useNavigator } from "hooks/useNavigator";
+import { CartContext } from "App";
+import { useTranslation } from "react-i18next";
 
 const styles = {
-  overflow: "hidden",
-  overflowWrap: "break-word",
-  height: "46px",
-}
+	overflow: "hidden",
+	overflowWrap: "break-word",
+	height: "46px",
+};
 
 export function OrderPage() {
-  const tele = window.Telegram.WebApp
-  const { t, i18n } = useTranslation()
-  const { env } = useNavigator()
-  const { cartItems, setCartItems } = useContext(CartContext)
-  const {foods, setFoods} = useContext(CartContext)
-  const { comment, setComment } = useContext(CartContext)
-  const { totalPrice, setTotalPrice } = useContext(CartContext)
-  const { queryId, setQueryId } = useContext(CartContext)
-  const { optionDelivery, setOptionDelivery } = useContext(CartContext)
-  const { address, setAddress } = useContext(CartContext)
-  const { telephone, setTelephone } = useContext(CartContext)
- 
-  
-  const handleChange = (event) => {
-    setComment(event.target.value)
-  }
-  
-  // useEffect(() => {
-  //   const user = tele.initDataUnsafe?.user
-  //   const queryIdTemp = tele.initDataUnsafe?.query_id
+	const tele = window.Telegram.WebApp;
+	const { t, i18n } = useTranslation();
+	const { env } = useNavigator();
+	const { cartItems, setCartItems } = useContext(CartContext);
+	const { foods, setFoods } = useContext(CartContext);
+	const { comment, setComment } = useContext(CartContext);
+	const { totalPrice, setTotalPrice } = useContext(CartContext);
+	const { queryId, setQueryId } = useContext(CartContext);
+	const { optionDelivery, setOptionDelivery } = useContext(CartContext);
+	const { address, setAddress } = useContext(CartContext);
+	const { telephone, setTelephone } = useContext(CartContext);
 
-  //   console.log('user', user)
-  //   console.log('queryIdTemp', queryIdTemp)
-  //   console.log('window.Telegram.WebApp.initDataUnsafe?.query_id', window.Telegram.WebApp.initDataUnsafe?.query_id)
+	const handleChange = (event) => {
+		setComment(event.target.value);
+	};
 
-  //   setQueryId(queryIdTemp)
-  //  }, [])
+	const navigate = useNavigate();
 
-  // const comment = location?.state?.comment
+	// const onBackButtonClicked = useCallback(() => {
+	//   navigate(-1)
+	// }, [cartItems])
 
-  // const totalPrice = location?.state?.totalPrice
-  // const totalPriceWithDiscount = (totalPrice * (1 - discount)).toFixed(2)
+	// tele.BackButton.onClick(onBackButtonClicked)
 
-  const navigate = useNavigate()
+	const onSubmit = useCallback(() => {
+		navigate("/payments");
+	}, [cartItems, comment]);
 
-  // const onBackButtonClicked = useCallback(() => {
-  //   navigate(-1)
-  // }, [cartItems])
+	useEffect(() => {
+		tele.onEvent("mainButtonClicked", onSubmit);
+		// tele.onEvent("backButtonClicked", onBackButtonClicked)
 
-  // tele.BackButton.onClick(onBackButtonClicked)
+		return () => {
+			tele.offEvent("mainButtonClicked", onSubmit);
+			// tele.offEvent("backButtonClicked", onBackButtonClicked)
+		};
+	}, [onSubmit]);
 
-  const onSubmit = useCallback(() => {
-    navigate("/payments")
-  }, [cartItems, comment])
+	useEffect(() => {
+		if (optionDelivery == "delivery" && !address) {
+			tele.MainButton.hide();
+		} else {
+			tele.MainButton.show();
+		}
+	}, [address]);
 
-  useEffect(() => {
-    tele.onEvent("mainButtonClicked", onSubmit)
-    // tele.onEvent("backButtonClicked", onBackButtonClicked)
+	const onChangeAddress = (e) => {
+		setAddress(e.target.value);
+	};
+	const onChangeTelephone = (e) => {
+		setTelephone(e.target.value);
+	};
 
-    return () => {
-      tele.offEvent("mainButtonClicked", onSubmit)
-      // tele.offEvent("backButtonClicked", onBackButtonClicked)
-    }
-  }, [onSubmit])
+	const onChangeOption = (e) => {
+		setOptionDelivery(e.target.value);
+	};
 
-  useEffect(() => {
-    if (optionDelivery == "delivery" && !address) {
-      tele.MainButton.hide()
-    } else {
-      tele.MainButton.show()
-    }
-  }, [address])
+	// useEffect(() => {
+	//   window.scrollTo({
+	//     top: document.body.scrollHeight,
+	//     behavior: "smooth",
+	//   })
+	// }, [optionDelivery])
 
-  const onChangeAddress = (e) => {
-    setAddress(e.target.value)
-  }
-  const onChangeTelephone = (e) => {
-    setTelephone(e.target.value)
-  }
+	useEffect(() => {
+		tele.BackButton.show();
+		tele.MainButton.setParams({ text: t("PAY") });
+	}, []);
 
-  const onChangeOption = (e) => {
-    setOptionDelivery(e.target.value)
-  }
+	const currentTimestamp = Math.floor(Date.now() / 1000);
 
-  // useEffect(() => {
-  //   window.scrollTo({
-  //     top: document.body.scrollHeight,
-  //     behavior: "smooth",
-  //   })
-  // }, [optionDelivery])
+	function calculateTotalPrice(products, order) {
+		let totalPrice = 0;
 
-  useEffect(() => {
-    tele.BackButton.show()
-    tele.MainButton.setParams({ text: t("PAY") })
-  }, [])
+		for (const item of order) {
+			const product = products.find((p) => p.id === item.id);
+			if (product) {
+				totalPrice += product.price * item.quantity;
 
-  const currentTimestamp = Math.floor(Date.now() / 1000)
-  
-  
-  
-  function calculateTotalPrice(products, order) {
-    let totalPrice = 0
+				if (item?.selectedToppings) {
+					for (const topping of item.selectedToppings) {
+						const toppingData = product.toppings.find((t) => t.title === topping);
+						if (toppingData) {
+							totalPrice += toppingData.price * item.quantity;
+						}
+					}
+				}
+			}
+		}
 
-    for (const item of order) {
-      const product = products.find((p) => p.id === item.id)
-      if (product) {
-        totalPrice += product.price * item.quantity
+		return totalPrice;
+	}
 
-        if (item?.selectedToppings) {
-          for (const topping of item.selectedToppings) {
-            const toppingData = product.toppings.find(
-              (t) => t.title === topping
-            )
-            if (toppingData) {
-              totalPrice += toppingData.price * item.quantity
-            }
-          }
-        }
-      }
-    }
+	useEffect(() => {
+		console.log("cartItems111", cartItems);
+		console.log("foods222", foods);
 
-    return totalPrice
-  }
+		setTotalPrice(calculateTotalPrice(foods, cartItems));
+	}, []);
 
-  useEffect(() => {
-    console.log('cartItems111', cartItems)
-    console.log('foods222', foods)
+	return (
+		<>
+			<div className="pageContainer">
+				<h1 className="title">{t("Order")}</h1>
+				<div className="orderContainer">
+					<div className="imageContainer">
+						<img src={orderImg} alt={"orderImg"} />
+					</div>
 
-    setTotalPrice(calculateTotalPrice(foods, cartItems))
-   }, [])
+					<div className="textContainer">
+						<div className="text1">
+							{" "}
+							{t("Order")} № {currentTimestamp}
+						</div>
+						{/* <div className="text1"> {t("Perfect lunch from Falafel Shop.")}</div> */}
+						{/* <div className="text_small">{`${discount}% ${t("discount")}`}</div> */}
+					</div>
+				</div>
 
-  
-  return (
-    <>
-      <div className="pageContainer">
-        <h1 className="title">{t("Order")}</h1>
-        <div className="orderContainer">
-          <div className="imageContainer">
-            <img src={orderImg} alt={"orderImg"} />
-          </div>
+				{cartItems && cartItems.length > 0 && (
+					<div className="cardsContainer">
+						{cartItems.map((food) => {
+							return <CardRowSmall food={food} key={food.id} />;
+						})}
+					</div>
+				)}
 
-          <div className="textContainer">
-            <div className="text1">
-              {" "}
-              {t("Order")} № {currentTimestamp}
-            </div>
-            {/* <div className="text1"> {t("Perfect lunch from Falafel Shop.")}</div> */}
-            {/* <div className="text_small">{`${discount}% ${t("discount")}`}</div> */}
-          </div>
-        </div>
-
-        {cartItems && cartItems.length > 0 && (
-          <div className="cardsContainer">
-            {cartItems.map((food) => {
-              return <CardRowSmall food={food} key={food.id} />
-            })}
-          </div>
-        )}
-
-        {/* <CardRowSmall
+				{/* <CardRowSmall
           key={9999}
           food={{ id: 9999, title: t("Free delivery"), textColor: "#4AF2A1" }}
         /> */}
-        {/* <CardRowSmall
+				{/* <CardRowSmall
           key={9998}
           food={{
             id: 9998,
@@ -190,95 +167,90 @@ export function OrderPage() {
             textColor: "#4AF2A1",
           }}
         /> */}
-        <CardRowSmall
-          key={9997}
-          food={{
-            id: 9997,
-            title: t("Total Price:"),
-            price: totalPrice,
-            // price: totalPriceWithDiscount,
-          }}
-        />
-      </div>
+				<CardRowSmall
+					key={9997}
+					food={{
+						id: 9997,
+						title: t("Total Price:"),
+						price: totalPrice,
+						// price: totalPriceWithDiscount,
+					}}
+				/>
+			</div>
 
-      <div className="comment_container">
-        <div className="title_mini"> {t("Your Comment :")} </div>
-     
-    
-    
-        <input
-          className="cafe-text-field js-order-comment-field cafe-block"
-          rows="1"
-          placeholder={t("Add comment")}
-          style={styles}
-          type="text"
-          value={comment}
-          onChange={handleChange}
-        />
+			<div className="comment_container">
+				<div className="title_mini"> {t("Your Comment :")} </div>
 
-        <div className="cafe-text-field-hint">
-          {t("Any special requests, details, final wishes etc.")}
-        </div>
-      </div>
-      
-   
-      
+				<input
+					className="cafe-text-field js-order-comment-field cafe-block"
+					rows="1"
+					placeholder={t("Add comment")}
+					style={styles}
+					type="text"
+					value={comment}
+					onChange={handleChange}
+				/>
 
-      <div className="form">
-        <div className="title_mini" style={{ color: "white" }}>
-          {t("Choose where you eat")}
-        </div>
+				<div className="cafe-text-field-hint">
+					{t("Any special requests, details, final wishes etc.")}
+				</div>
+			</div>
 
-        <FormControl className="select" style={{ borderColor: "white", color: "white" }}>
-          <InputLabel style={{ borderColor: "white", color: "white" }}>
-            {t("Select Delivery Option")}
-          </InputLabel>
-          <Select
-            value={optionDelivery}
-            onChange={onChangeOption}
-            sx={{
-              "color": "white",
-              ".MuiOutlinedInput-notchedOutline": {
-                borderColor: "rgb(255, 255, 255)",
-              },
-              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                borderColor: "rgb(255, 255, 255)",
-              },
-              "&:hover .MuiOutlinedInput-notchedOutline": {
-                borderColor: "rgb(255, 255, 255)",
-              },
-              ".MuiSvgIcon-root ": {
-                fill: "white !important",
-              },
-            }}
-            labelId="select-filter-by-field-labe;"
-            id="select-filter-by-field"
-          >
-            <MenuItem value="on_site">{t("On Site")}</MenuItem>
-            <MenuItem value="take_away">{t("Take Away")}</MenuItem>
-            <MenuItem value="delivery">{t("Delivery")}</MenuItem>
-          </Select>
-        </FormControl>
+			<div className="form">
+				<div className="title_mini" style={{ color: "white" }}>
+					{t("Choose where you eat")}
+				</div>
 
-        {optionDelivery === "delivery" && (
-          <Box>
-            <StyledTextField
-              label={t("Address")}
-              value={address}
-              onChange={onChangeAddress}
-              placeholder={t("Enter address")}
-            />
-            <StyledTextField
-              label={t("Telephone")}
-              value={telephone}
-              onChange={onChangeTelephone}
-              placeholder={t("Enter Telephone")}
-            />
-          </Box>
-        )}
-      </div>
+				<FormControl className="select" style={{ borderColor: "white", color: "white" }}>
+					<InputLabel style={{ borderColor: "white", color: "white" }}>
+						{t("Select Delivery Option")}
+					</InputLabel>
+					<Select
+						value={optionDelivery}
+						onChange={onChangeOption}
+						sx={{
+							"color": "white",
+							".MuiOutlinedInput-notchedOutline": {
+								borderColor: "rgb(255, 255, 255)",
+							},
+							"&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+								borderColor: "rgb(255, 255, 255)",
+							},
+							"&:hover .MuiOutlinedInput-notchedOutline": {
+								borderColor: "rgb(255, 255, 255)",
+							},
+							".MuiSvgIcon-root ": {
+								fill: "white !important",
+							},
+						}}
+						labelId="select-filter-by-field-labe;"
+						id="select-filter-by-field"
+					>
+						<MenuItem value="on_site">{t("On Site")}</MenuItem>
+						<MenuItem value="take_away">{t("Take Away")}</MenuItem>
+						<MenuItem value="delivery">{t("Delivery")}</MenuItem>
+					</Select>
+				</FormControl>
 
-      {/* {env === "browser" && (
+				{optionDelivery === "delivery" && (
+					<Box>
+						<StyledTextField
+							label={t("Address")}
+							value={address}
+							onChange={onChangeAddress}
+							placeholder={t("Enter address")}
+						/>
+						<StyledTextField
+							label={t("Telephone")}
+							value={telephone}
+							onChange={onChangeTelephone}
+							placeholder={t("Enter Telephone")}
+						/>
+					</Box>
+				)}
+			</div>
+
+			{/* {env === "browser" && (
         <BigButton
           title={t("Payments")}
           type={"payments"}
@@ -287,11 +259,11 @@ export function OrderPage() {
         />
       )} */}
 
-      {env == "browser" && (
-        <BigButton title={`${`Buy ${totalPrice} ₪`} `} onClick={onSubmit} />
-      )}
+			{env == "browser" && (
+				<BigButton title={`${`Buy ${totalPrice} ₪`} `} onClick={onSubmit} />
+			)}
 
-      {/* {env === "browser" &&
+			{/* {env === "browser" &&
         (optionDelivery === "on_site" ||
           (optionDelivery === "take_away" && address)) && (
           <BigButton
@@ -301,6 +273,6 @@ export function OrderPage() {
             onClick={onSubmit}
           />
         )} */}
-    </>
-  )
+		</>
+	);
 }
